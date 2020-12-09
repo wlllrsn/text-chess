b91c26aafac0820e0af98e35187483e8d5ad7707
WillLarsonWM <wplarson@email.wm.edu>
2020-12-09 15:09:02 -0500

    All attributes are now handled correctly, with/without FEN usage

--- Board.py
+++ Board.py
@@ -1,6 +1,16 @@
-# A class that contains a chess board, with piece positions
+"""
+A class that contains a chess board, with piece positions
+The class can be initialized in multiple ways:
+Default: The board is given the typical chess starting position and all attributes are given accordingly
+Manual: Using the parameters in the constructor, each attribute can be controlled individually when the class is 
+        instantiated. This requires an 8x8 list for the board position and correct values for the other attributes.
+        Probably the most difficult method. Might phase out in the future.
+Automatic: Pass the class a valid FEN string when created and all values will be filled out automatically based on
+           string.
+"""
 
 from defaultBoardPositions import *
+
 
 class Board:
     def __init__(self, starting_position=STARTING_POSITION, castling='KQkq', whiteTurn=True, enPassant='-', halfMove=0,
@@ -8,7 +18,15 @@ class Board:
 
         # The two-dimensional list that holds the positions of the pieces. By default it is the regular starting
         # locations of the pieces
-        self.positions = starting_position
+        if type(starting_position) == list:
+            self.positions = starting_position
+        elif type(starting_position) == str:
+            self.positions = EMPTY_POSITION
+            self.__boardFromFEN(starting_position)
+        else:
+            print('Invalid board position given. Board is given the default starting position.\n')
+            self.positions = STARTING_POSITION
+            return
 
         # attribute that describes whether each side can castle
         self.castling = castling
@@ -25,10 +43,13 @@ class Board:
         # full move counter - used for the 50 move rule
         self.fullMoveCounter = fullMove
 
-
     # method that sets all attributes of the board from a given FEN string
-    def boardFromFEN(self, FEN):
+    def __boardFromFEN(self, FEN):
         FEN_list = FEN.split()
+        if len(FEN_list) != 6:
+            print('Invalid FEN string given. Board is given the default starting position.\n')
+            self.positions = STARTING_POSITION
+            self.__giveAttributesDefaultValues()
 
         # set piece positions
         row = 0
@@ -59,6 +80,14 @@ class Board:
         # set half and full move counters
         self.halfMoveCounter = int(FEN_list[4])
         self.fullMoveCounter = int(FEN_list[5])
+
+    # used by boardFromFEN method when given an invalid FEN string
+    def __giveAttributesDefaultValues(self):
+        self.castling = 'KQkq'
+        self.whiteTurn = True
+        self.enPassantTarget = '-'
+        self.halfMoveCounter = 0
+        self.fullMoveCounter = 1
 
     def __str__(self):
         string = '+ - + - + - + - + - + - + - + - +\n'
--- ChessMain.py
+++ ChessMain.py
@@ -3,8 +3,8 @@
 from Board import Board
 from defaultBoardPositions import *
 
-board1 = Board()
+board1 = Board(123)
 
-board1.boardFromFEN('rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1')
+# board1.boardFromFEN('rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1')
 
 print(board1)
